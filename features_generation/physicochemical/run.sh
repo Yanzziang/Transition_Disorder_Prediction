bedf9615b25ff1470f9d4a662df21d7d2a464dfb
Yanzziang <yzza.steven@outlook.jp>
2020-01-31 20:52:11 +0900

    del

--- features_generation/physicochemical/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-filelist=`ls /Users/yzza/disorder-dataset/`
-
-for file in $filelist
-do
-    python3 /Users/yzza/disorder-dataset/phschem/phsicochemical.py /Users/yzza/disorder-dataset/$file
-done
