6bc4c428cbb4bc3a6786c8d27735b56c8ec21f6a
Yan Ziang <39987917+Yanzziang@users.noreply.github.com>
2020-03-13 20:28:18 +0900

    update run_local.sh
    revise the mv option

--- features_generation/SPIDER2/scripts/run_local.sh
+++ features_generation/SPIDER2/scripts/run_local.sh
@@ -18,12 +18,10 @@ for seq1 in $*; do
 	#echo $(basename $seq1 .seq)
 	pro1=$(basename $(basename $seq1 .fasta) .pssm)
 
-	[ -f $xdir/../ss_file/$pro1.spd3 ] && continue
 	if [ ! -f $xdir/../../pssm/$pro1.pssm ]; then
 		$blastpgp -db $NR -num_iterations 3 -num_alignments 1 -num_threads 8 -query $seq_di/$pro1.fasta -out_ascii_pssm $xdir/../../pssm/$pro1.pssm > /dev/null
 	fi
 	#echo $xdir
 	$xdir/pred_pssm.py $xdir/../../pssm/$pro1.pssm
+	mv $xdir/../$pro1.spd3 $xdir/../ss_file/
 done
-
-mv $xdir/../$pro1.spd3 $xdir/../ss_file/
